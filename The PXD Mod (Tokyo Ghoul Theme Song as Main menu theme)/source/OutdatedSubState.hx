6c404030049b222706b8b3950218790f327d6c3d
WhittyFNF <81534469+WhittyFNF@users.noreply.github.com>
2021-06-18 16:46:41 +0100

    DEV Release

---
+++ The PXD Mod (Tokyo Ghoul Theme Song as Main menu theme)/source/OutdatedSubState.hx
@@ -0,0 +1,44 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.RED);
+		add(bg);
+		var ver = "v" + Application.current.meta.get('version');
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Ok This is the PXD Version 0.0.1 !\nHope you guys enjoy this "
+			+ " I'm guessing you want to tryout so "
+			+ "! We waiting for you to tryout!!",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.YELLOW, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
